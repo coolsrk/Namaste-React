ce10155b93518bdbd8fdf4c140439c4d0fd9fb05
coolsrk <kulkarnishreyasr@gmail.com>
2023-04-06 23:39:29 +0530

    Added more child elements.

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
-const heading = React.createElement("div", {id: "parent"},
-[React.createElement("h1", {id: 'child1'}, "Its h1 tag"), 
-React.createElement("h2", {id: "child2"} , "Its the h2.")]);
+const heading = React.createElement("div", {
+        id: "parent"
+    },
+    [React.createElement("div", {
+        id: 'parent1'
+    }, [React.createElement("h1", {
+        id: "child1"
+    }, "Its the h1."), React.createElement("h2", {
+        id: "child2"
+    }, "Its the h2.")]), 
+    React.createElement("div", {
+        id: 'parent2'
+    }, [React.createElement("h1", {
+        id: "child1"
+    }, "Its the second h1."), React.createElement("h2", {
+        id: "child2"
+    }, "Its the second h2.")]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
