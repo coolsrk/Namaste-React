54f5f18bfbd3ad4d5955d2546e95b75b8878cda6
coolsrk <kulkarnishreyasr@gmail.com>
2023-11-15 12:37:59 +0530

    Lecture 6: Added Search, top rated functionality along with Shimmer UI. Used useEffect() hook.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,70 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Restaurant } from "./Restaurant";
-import * as RESTAURANTS from "../restaurent-details.json";
+// import * as RESTAURANTS from "../restaurent-details.json";
+import { Shimmer } from "./Shimmer";
+
+// let cardsData = [];
 
 export const Body = () => {
-  const [resList, giveTopRated] = useState(RESTAURANTS.restaurants);
+  const [resList, setRestroList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+
+  const [text, setText] = useState("");
+  // console.log(text);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    setFilteredList(json?.data?.cards[5].card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setRestroList(
+      json?.data?.cards[5].card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  // Conditional rendering
+  if (resList.length === 0) {
+    return <Shimmer />;
+  }
+
   return (
     <div className="body">
-      <button className="search" onClick={() => {
-        const res = resList.filter((v) => v.info.avgRating > 4.2)
-        console.log(res);
-        giveTopRated(res);
-      }}>
-        Search
-      </button>
+      <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+
+        <button
+          onClick={() => {
+            const searchList = resList.filter((e) => e.info.name.toLowerCase().includes(text.toLowerCase()));
+            setFilteredList(searchList);
+          }}
+        >
+          Search
+        </button>
+
+        <button
+          onClick={() => {
+            const res = resList?.filter((v) => v.info.avgRating > 4.2);
+            setFilteredList(res);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="res-container">
-        {resList.map((value) => (
+        {filteredList?.map((value) => (
           <Restaurant resData={value.info} key={value.info.id} />
         ))}
       </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,11 @@
+export const Shimmer = () => {
+  return (
+    <div className="shimmer-ui-container">
+      <div className="shimmer-ui"></div>
+      <div className="shimmer-ui"></div>
+      <div className="shimmer-ui"></div>
+      <div className="shimmer-ui"></div>
+      <div className="shimmer-ui"></div>
+    </div>
+  );
+};
